880eca83a05ed9f55d08bc7f38259a5aebb71dc6
Christian <christiandave120702@gmail.com>
2025-10-21 19:06:48 +0800

    feat: add useViewArchiveItemDetails hook for fetching details of archived items

---
+++ src/query/get/useViewArchiveItemDetails.ts
@@ -0,0 +1,32 @@
+import { queryOptions } from "@tanstack/react-query";
+import { getToken } from "../../utils/token";
+
+const viewArchiveItemDetails = async (id: string) => {
+    try {
+        const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+        const END_POINT = "/api/v1/archiveitems";
+
+        const res = await fetch(`${BASE_URL}${END_POINT}/${id}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${getToken()}`,
+            },
+        });
+
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.message || "Archived item details not found");
+
+        return data.data;
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const useViewArchiveItemDetails = (id: string) => {
+    return queryOptions({
+        queryKey: ["ArchiveItem", id],
+        queryFn: () => viewArchiveItemDetails(id),
+    });
+};
