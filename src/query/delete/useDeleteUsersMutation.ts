dd8f0dcd7b80d3bd1dad4cc3b198ca56df2fd6e4
Christian <christiandave120702@gmail.com>
2025-10-30 17:20:53 +0800

    feat: update the endpoint and assign the version in to var for readability

---
+++ src/query/delete/useDeleteUsersMutation.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getToken } from "../../utils/token";
+
+const useDeleteUser = async (id: string) => {
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const VERSION = "v1";
+  const END_POINT = `/api/${VERSION}/archiveusers/permanent-delete`;
+
+  const res = await fetch(`${BASE_URL}${END_POINT}${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${getToken()}`
+    }
+  });
+
+  const data = await res.json()
+  if (!res.ok) throw new Error(data.message || "Delete User Failed");
+
+  return data;
+}
+
+export const useDeleteUserMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["permanent-delete"],
+    mutationFn: useDeleteUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["ArchiveUsers"] })
+    }
+  })
+}
