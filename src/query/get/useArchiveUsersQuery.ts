d7daa17945de7408c571870413bb4399e91a8d6f
Christian <christiandave120702@gmail.com>
2025-10-30 17:22:24 +0800

    refactor: update the endpoint and assign the version in to var for readability

--- src/query/get/useArchiveUsersQuery.ts
+++ src/query/get/useArchiveUsersQuery.ts
@@ -4,7 +4,8 @@ import { getToken } from "../../utils/token";
 const ArchivesUsers = async () => {
   try {
     const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-    const END_POINT = "/api/ArchiveUsers";
+    const VERSION = "v1";
+    const END_POINT = `/api/${VERSION}/archiveusers`;
 
     const res = await fetch(`${BASE_URL}${END_POINT}`, {
       method: "GET",
