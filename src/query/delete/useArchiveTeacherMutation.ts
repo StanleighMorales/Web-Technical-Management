8eed27933b5043f8fe58e771398db2f53e41dc39
Christian <christiandave120702@gmail.com>
2025-10-30 17:18:59 +0800

    refactor: update the endpoint and assign the version in to var for readability

--- src/query/delete/useArchiveTeacherMutation.ts
+++ src/query/delete/useArchiveTeacherMutation.ts
@@ -3,8 +3,9 @@ import { getToken } from "../../utils/token";
 
 const archiveTeacher = async (teacherId: string) => {
   const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-  const END_POINT = "/api/v1/users/archive";
-  
+  const VERSION = "v1";
+  const END_POINT = `/api/${VERSION}/users/archive`;
+
   const res = await fetch(`${BASE_URL}${END_POINT}/${teacherId}`, {
     method: "DELETE",
     headers: {
