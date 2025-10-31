86ac2c71f0ae6ad2186d7206ca876dd944f07c4e
Christian <christiandave120702@gmail.com>
2025-10-31 20:39:39 +0800

    refactor: update the endpoint and assign the version in to var for readability and remove the staleTime in the query fn

--- src/query/get/useUserQuery.ts
+++ src/query/get/useUserQuery.ts
@@ -1,9 +1,11 @@
 import { getToken } from "../../utils/token";
 import { queryOptions } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 const fetchUserData = async () => {
   const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-  const END_POINT = "/api/v1/auth/me";
+  const VERSION = "v1";
+  const END_POINT = `/api/${VERSION}/auth/me`;
 
   const res = await fetch(`${BASE_URL}${END_POINT}`, {
     method: "GET",
@@ -23,6 +25,5 @@ export const useUserQuery = () => {
   return queryOptions({
     queryKey: ["me"],
     queryFn: fetchUserData,
-    staleTime: Infinity,
   });
 };
