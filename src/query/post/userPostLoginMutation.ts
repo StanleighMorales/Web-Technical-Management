43f3c148a170595acb3fbb8dac779ce7d0de29b8
Christian <christiandave120702@gmail.com>
2025-10-31 20:42:13 +0800

    refactor: update the endpoint and assign the version in to var for readability

--- src/query/post/userPostLoginMutation.ts
+++ src/query/post/userPostLoginMutation.ts
@@ -5,7 +5,8 @@ const LoginUser = async (formData: TLoginUser) => {
 
   try {
     const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-    const END_POINT = "/api/v1/auth/login";
+    const VERSION = "v1";
+    const END_POINT = `/api/${VERSION}/auth/login`;
 
     const userFormData = JSON.stringify(formData)
     const res = await fetch(`${BASE_URL}${END_POINT}`, {
