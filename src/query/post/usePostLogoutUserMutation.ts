749b423c1a44508129f21b52090febcd091f88d9
Christian <christiandave120702@gmail.com>
2025-10-31 20:42:04 +0800

    refactor: update the endpoint and assign the version in to var for readability

--- src/query/post/usePostLogoutUserMutation.ts
+++ src/query/post/usePostLogoutUserMutation.ts
@@ -4,8 +4,9 @@ import { removeToken } from './../../utils/token/index';
 const LogoutUser = async () => {
     try {
         const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-        const END_POINT = "/api/v1/auth/logout";
-        
+        const VERSION = "v1";
+        const END_POINT = `/api/${VERSION}/auth/logout`;
+
         const res = await fetch(`${BASE_URL}${END_POINT}`, {
             method: "POST",
             credentials: "include"
