97bd4b615e688db45889d1088c43fa626e98a88a
Christian <christiandave120702@gmail.com>
2025-11-10 17:26:00 +0800

    refactor: update the attributes of the recent borrowed items and also update the ui design

--- src/components/RecentBorrowedItemsTable.tsx
+++ src/components/RecentBorrowedItemsTable.tsx
@@ -1,32 +1,80 @@
+import { MdVisibility } from "react-icons/md";
 import { SlugStatus } from "./SlugStatus";
-import type { TRecentBorrowedItemsTableProps } from "../types/types";
-export default function RecentBorrowedItemsTable({
-  id,
-  datetime,
-  teacher,
+import type { FC } from "react";
+import { FormattedDateTime } from "./FormatedDateTime";
+
+type TRecentBorrowedItemsTableProps = {
+  image: null;
+  itemName: string;
+  serialNumber: string;
+  barcode?: null;
+  barcodeImage?: null;
+  borrowerFullName: string;
+  room: string;
+  teacherFullName: string;
+  subjectTimeSchedule?: string;
+  remarks: string | null;
+  createdAt: string;
+  onSetSelectedId?: (value: string) => void;
+  onView?: (value: boolean) => void;
+}
+export const RecentBorrowedItemsTable: FC<TRecentBorrowedItemsTableProps> = ({
+  image,
+  itemName,
+  serialNumber,
+  borrowerFullName,
   room,
-  item,
-  occupied,
-  status,
-}: TRecentBorrowedItemsTableProps) {
+  teacherFullName,
+  remarks,
+  createdAt,
+  onSetSelectedId,
+  onView
+}) => {
+
+  const handleviewBorrowItemDetails = (id: string) => {
+    if (onSetSelectedId && onView) {
+      onSetSelectedId(id);
+      onView(true);
+    }
+  }
+
   return (
     <>
-      <td hidden>{id}</td>
-      <td className="py-3 px-6">{datetime}</td>
-      <td className="py-3 px-6">
-        {teacher.charAt(0).toUpperCase() + teacher.slice(1)}
+      <td className="py-3 px-4">
+        {serialNumber}
       </td>
-      <td className="py-3 px-6">
-        {room.charAt(0).toUpperCase() + room.slice(1)}
+      <td className="py-4 px-6">
+        <img
+          src={typeof image === "string" ? image : "-"}
+          alt={itemName}
+          className="w-10 h-10 rounded-xl"
+        />
       </td>
-      <td className="py-3 px-6">
-        {item.charAt(0).toUpperCase() + item.slice(1)}
+      <td className="py-4 px-6">
+        {itemName}
       </td>
-      <td className="py-3 px-6">
-        {occupied.charAt(0).toUpperCase() + occupied.slice(1)}
+      <td className="py-4 px-6">
+        {borrowerFullName}
       </td>
-      <td className="py-3 px-6">
-        {SlugStatus(status.charAt(0).toUpperCase() + status.slice(1))}
+      <td className="py-4 px-6">
+        {teacherFullName || "-"}
+      </td>
+      <td className="py-4 px-6">
+        {room || "-"}
+      </td>
+      <td className="py-4 px-6">
+        {remarks}
+      </td>
+      <td className="py-4 px-6">
+        {FormattedDateTime(createdAt)}
+      </td>
+      <td className="py-4 px-6 font-semibold text-center">
+        <button onClick={() => handleviewBorrowItemDetails(serialNumber)}
+          className="mr-2 text-2xl text-green-500"
+          title="View more"
+        >
+          <MdVisibility />
+        </button>
       </td>
     </>
   );
