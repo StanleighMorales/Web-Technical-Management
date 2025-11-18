f5978ae99333a525d93ad397376930146bacab25
Christian <christiandave120702@gmail.com>
2025-11-18 22:11:14 +0800

    style: update the UI design for better design also the mobile view

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,7 @@ type SearchProps = {
 
 const SearchBar = ({ onChangeValue, name, placeholder }: SearchProps) => {
   return (
-    <div className="flex items-center py-2 px-4 mb-6 w-full max-w-md rounded-xl shadow-inner bg-[#f1f5f9]">
+    <div className="flex items-center py-2 px-4 mb-6 w-full max-w-md rounded-md shadow-inner bg-[#f1f5f9]">
       <FaSearch className="mr-3 text-xl text-[#64748b]" />
       <input
         className="p-1 w-full text-lg bg-transparent border-none outline-none text-[#222] placeholder-[#94a3b8]"
