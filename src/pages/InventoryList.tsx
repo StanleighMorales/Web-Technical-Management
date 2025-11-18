7b31213cd8472b80967e0a35c8c4c79e2f3c662e
Christian <christiandave120702@gmail.com>
2025-11-18 21:35:29 +0800

    refactor: use the Success Alert component for better user experience and the the excel import feature for the item

--- src/pages/InventoryList.tsx
+++ src/pages/InventoryList.tsx
@@ -12,15 +12,18 @@ import { InventoryBadges } from "../components/InventoryBadges";
 import Pagination from "../components/Pagination";
 import { InventoryTable } from "../components/InventoryTable";
 import ErrorTable from "../components/ErrorTables";
+import ExcelImportButton from "../components/ExcelImportButton";
+import { SuccessAlert } from "../components/SuccessAlert";
 
 export default function InventoryList() {
+  const [ShowAlert, setShowAlert] = useState<boolean>(false);
+  const [ShowMessage, setShowMessage] = useState<string>("");
   const [isAddItemFormOpen, setIsAddItemFormOpen] = useState(false);
   const [searchItem, setSearchItem] = useState<string>("");
   const [selectedCategory, setSelectedCategory] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 10;
   const [items, setItems] = useState<TItemList[]>([]);
-
 
   // this func use a useMemo to filtered item either its itemName or the Category and also for the Matches Category and return items,searchItem and selectedCategory
   const filteredItems = useMemo(
@@ -36,7 +39,7 @@ export default function InventoryList() {
 
         return matchesSearch && matchesCategory;
       }),
-    [items, searchItem, selectedCategory],
+    [items, searchItem, selectedCategory]
   );
   const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
 
@@ -48,9 +51,9 @@ export default function InventoryList() {
     () =>
       filteredItems.slice(
         (validCurrentPage - 1) * itemsPerPage,
-        validCurrentPage * itemsPerPage,
+        validCurrentPage * itemsPerPage
       ),
-    [filteredItems, itemsPerPage, validCurrentPage],
+    [filteredItems, itemsPerPage, validCurrentPage]
   );
 
   // this func will handle all the page triggered in the button to set either to 1 to 2 or 3 etc
@@ -64,7 +67,7 @@ export default function InventoryList() {
       setSelectedCategory(selectedCategory === category ? "" : category);
       setCurrentPage(1);
     },
-    [selectedCategory],
+    [selectedCategory]
   );
 
   // this func return to display all the items when the selectedCategory is executed
@@ -90,8 +93,9 @@ export default function InventoryList() {
 
   return (
     <div className="flex flex-col w-full antialiased bg-linear-to-br animate-fadeIn inventory-list-container min-h-svh from-[#f8fafc] via-[#e8eef7] to-[#dbeafe]">
+      {ShowAlert && <SuccessAlert message={ShowMessage} />}
       <header className="flex sticky top-0 z-30 flex-col items-center px-8 pt-8 pb-6 border-b shadow-sm inventory-header bg-white/70 backdrop-blur-md border-[#e5e9f2]">
-        <h1 className="mb-2 text-5xl font-extrabold tracking-tight text-[#1e293b] drop-shadow-lg">
+        <h1 className="mb-2 text-5xl mt-10 lg:mt-0 md:mt-0 font-extrabold tracking-tight text-[#1e293b] drop-shadow-lg">
           Inventory List
         </h1>
         <p className="max-w-2xl text-base font-medium text-center md:text-lg text-[#64748b]">
@@ -103,11 +107,11 @@ export default function InventoryList() {
       <div className="overflow-auto h-full">
         {/* Inventory Stats */}
         <section className="py-6 px-8 mx-auto w-full scrollbar-none">
-          <div className="flex overflow-x-auto gap-3 pb-2 w-full scrollbar-none">
+          <div className="flex flex-row overflow-x-auto gap-3 pb-2 w-full scrollbar-none">
             {Array.from(new Set(items.map((item) => item.category))).map(
               (category) => {
                 const itemsInCategory = items.filter(
-                  (item) => item.category === category,
+                  (item) => item.category === category
                 );
                 return (
                   <InventoryBadges
@@ -118,7 +122,7 @@ export default function InventoryList() {
                     isSelected={selectedCategory === category}
                   />
                 );
-              },
+              }
             )}
           </div>
         </section>
@@ -126,17 +130,20 @@ export default function InventoryList() {
         {/* Inventory Items Table */}
         <section className="px-8">
           <div className="overflow-x-auto p-4 rounded-2xl ring-1 shadow-xl bg-white/90 h-[60vh] ring-[#e0e7ef]/80">
-            <section className="flex flex-col gap-3 justify-between mb-4 lg:md:flex-row">
-              <div>
-                <Button
-                  onClick={() => setIsAddItemFormOpen(true)}
-                  name={"New Item"}
-                />
+            <section className="flex flex-col gap-3 justify-between mb-4 lg:flex-row md:flex-row">
+              <div className="flex flex-row gap-4">
+                <div>
+                  <Button
+                    onClick={() => setIsAddItemFormOpen(true)}
+                    name={"New Item"}
+                  />
+                </div>
+                <ExcelImportButton />
               </div>
               <div className="flex flex-col gap-2 items-center md:flex-row lg:flex-row">
                 {/*Pagination Component*/}
                 {filteredItems.length > 0 && (
-                  <div className="flex pt-2 -ml-20">
+                  <div className="flex pt-2 -ml-30">
                     <Pagination
                       totalPages={totalPages}
                       currentPage={currentPage}
@@ -195,7 +202,7 @@ export default function InventoryList() {
                       paginatedData.map((item) => (
                         <tr
                           key={item.serialNumber}
-                          className="transition-colors cursor-pointer odd:bg-white even:bg-[#f9fbff] hover:bg-[#f8fafc]"
+                          className="transition-colors cursor-pointer py-4 odd:bg-white even:bg-[#f9fbff] hover:bg-[#f8fafc]"
                         >
                           <InventoryTable
                             id={item.id}
@@ -207,7 +214,18 @@ export default function InventoryList() {
                             Category={item.category}
                             Condition={item.condition}
                             Status={item.status}
-                            onMutate={() => mutate(item.id)}
+                            onMutate={() =>
+                              mutate(item.id, {
+                                onSuccess: (data) => {
+                                  setShowAlert(true);
+                                  setShowMessage(data.message);
+                                  setTimeout(() => {
+                                    setShowAlert(false);
+                                    setShowMessage("");
+                                  }, 3500);
+                                },
+                              })
+                            }
                           />
                         </tr>
                       ))
@@ -223,7 +241,8 @@ export default function InventoryList() {
                       No items found
                     </h3>
                     <p className="text-base text-[#64748b]">
-                      Try adjusting your search or filters. New items will appear here once created.
+                      Try adjusting your search or filters. New items will
+                      appear here once created.
                     </p>
                   </div>
                 </div>
