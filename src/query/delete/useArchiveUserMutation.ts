3f5c645001a3f052785f3c324e80045cb944055b
Christian <christiandave120702@gmail.com>
2025-11-18 21:38:37 +0800

    refactor: instead use the normal message change into the actual message from response

--- src/query/delete/useArchiveUserMutation.ts
+++ src/query/delete/useArchiveUserMutation.ts
@@ -20,13 +20,13 @@ const ArchiveUser = async (id: string) => {
     }
 
     const data = await res.json();
-    if (!res.ok) {
-      throw new Error("Failed to delete item");
-    }
+    if (!res.ok) throw new Error(data.errors);
 
     return data;
-  } catch {
-    return null;
+  } catch(error) {
+    if(error instanceof Error){
+      console.log(error)
+    }
   }
 };
 
