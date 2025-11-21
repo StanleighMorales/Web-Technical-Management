0e281da9e2946c9bb593e5e72895b6061955b6cd
Christian <christiandave120702@gmail.com>
2025-11-22 06:33:57 +0800

    feat: add a component named show alert for success and error for better user experience

--- src/components/ExcelImportItemButton.tsx
+++ src/components/ExcelImportItemButton.tsx
@@ -6,75 +6,75 @@ import { SuccessAlert } from "./SuccessAlert";
 import { ErrorAlert } from "./ErrorAlert";
 
 export default function ExcelImportItemButton() {
-  const [showAlertSuccess, setShowAlertSuccess] = useState<boolean>(false);
-  const [showAlertFailed, setShowAlertFailed] = useState<boolean>(false);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [fileData, setFileData] = useState<File | null>(null);
+    const [showAlertSuccess, setShowAlertSuccess] = useState<boolean>(false);
+    const [showAlertFailed, setShowAlertFailed] = useState<boolean>(false);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+    const [fileData, setFileData] = useState<File | null>(null);
 
-  const { mutate: importItem } = usePostImportMutation();
+    const { mutate: importItem } = usePostImportMutation();
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
 
-    setFileData(file);
-    console.log(fileData)
+        setFileData(file);
+        console.log(fileData)
 
-    const reader = new FileReader();
+        const reader = new FileReader();
 
-    reader.onload = (event) => {
-      const arrayBuffer = event.target?.result;
-      if (!arrayBuffer) return;
+        reader.onload = (event) => {
+            const arrayBuffer = event.target?.result;
+            if (!arrayBuffer) return;
 
-      const data = new Uint8Array(arrayBuffer as ArrayBuffer);
-      const workbook = XLSX.read(data, { type: "array" });
+            const data = new Uint8Array(arrayBuffer as ArrayBuffer);
+            const workbook = XLSX.read(data, { type: "array" });
 
-      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      XLSX.utils.sheet_to_json(worksheet);
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+            XLSX.utils.sheet_to_json(worksheet);
 
+        };
+
+        reader.readAsArrayBuffer(file);
+
+        const form = new FormData();
+        form.append("file", file);
+
+        importItem(form, {
+            onSuccess: () => {
+                setShowAlertSuccess(true);
+                setTimeout(() => {
+                    setShowAlertSuccess(false);
+                }, 3500);
+            },
+            onError: (error) => {
+                console.error(error.message);
+                setShowAlertFailed(true);
+                setTimeout(() => {
+                    setShowAlertFailed(false);
+                }, 3500);
+            },
+        });
     };
 
-    reader.readAsArrayBuffer(file);
+    return (
+        <div>
+            {showAlertSuccess && <SuccessAlert message="Excel imported successfully!" />}
+            {showAlertFailed && <ErrorAlert message="Excel imported failed" />}
 
-    const form = new FormData();
-    form.append("file", file);
+            <input
+                type="file"
+                accept=".xlsx,.xls"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                className="hidden"
+            />
 
-    importItem(form, {
-      onSuccess: () => {
-        setShowAlertSuccess(true);
-        setTimeout(() => {
-          setShowAlertSuccess(false);
-        }, 1500);
-      },
-      onError: (error) => {
-        console.error(error.message);
-        setShowAlertFailed(true);
-        setTimeout(() => {
-          setShowAlertFailed(false);
-        }, 3000);
-      },
-    });
-  };
-
-  return (
-    <div>
-      {showAlertSuccess && <SuccessAlert message="Excel imported successfully!" />}
-      {showAlertFailed && <ErrorAlert message="Excel imported failed" />}
-
-      <input
-        type="file"
-        accept=".xlsx,.xls"
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        className="hidden"
-      />
-
-      <button
-        onClick={() => fileInputRef.current?.click()}
-        className="flex items-center gap-2 px-5 py-2.5 cursor-pointer bg-green-500 text-white font-semibold rounded-md shadow-md hover:scale-100 hover:shadow-sm transition-all duration-150"
-      >
-        <FaFileImport className="text-md font-bold mr-1" /> Import Item
-      </button>
-    </div>
-  );
+            <button
+                onClick={() => fileInputRef.current?.click()}
+                className="flex items-center gap-2 px-5 py-2.5 cursor-pointer bg-green-500 text-white font-semibold rounded-md shadow-md hover:scale-100 hover:shadow-sm transition-all duration-150"
+            >
+                <FaFileImport className="text-md font-bold mr-1" /> Import Items
+            </button>
+        </div>
+    );
 }
