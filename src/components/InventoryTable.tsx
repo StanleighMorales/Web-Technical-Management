451b3a74690959503447a4c955fa45d5588e0597
Christian <christiandave120702@gmail.com>
2025-11-22 14:30:47 +0800

    refactor: fix the bug on clicking the icon and tr table and also remove the unused icon

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -1,14 +1,13 @@
 import type { FC } from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { IoArchive } from "react-icons/io5";
 import logo from "../assets/img/aclcLogo.webp";
 import { FormattedDateTime } from "./FormattedDateTime";
 import { SlugCondition } from "./SlugCondition";
 import { SlugStatus } from "./SlugStatus";
-import { MdVisibility } from "react-icons/md";
 import { UserData } from "../utils/usersData/userData";
 import PopUpModal from "./PopUpModal";
+import { useNavigate } from "react-router-dom";
 
 type checkIfUserAdminProps = {
     userRole?: string;
@@ -29,6 +28,7 @@ type InventoryTableProps = {
 };
 
 export const InventoryTable: FC<Required<InventoryTableProps>> = (props) => {
+    const navigate = useNavigate();
     const [isConfirmOpen, setIsConfirmOpen] = useState<boolean>(false);
     const data = UserData();
     const userRole = data.userRole;
@@ -63,54 +63,52 @@ export const InventoryTable: FC<Required<InventoryTableProps>> = (props) => {
 
     return (
         <>
-            <td className="py-3 px-4">{props.SerialNumber}</td>
-            <td className="py-3 px-4">
-                <img
-                    src={typeof props.Image === "string" ? props.Image : logo}
-                    alt={props.ItemName}
-                    className="w-10 h-10 rounded-xl"
-                />
-            </td>
-            <td className="py-3 px-4">{props.ItemName}</td>
-            <td className="py-3 px-4">{props.Category}</td>
-            <td className="py-3 px-4">
-                <span
-                    className={`px-3 py-1 rounded-full text-sm ${SlugCondition(props.Condition)}`}
-                >
-                    {props.Condition}
-                </span>
-            </td>
-            <td className="py-3 px-4">{FormattedDateTime(props.createdAt)}</td>
-            <td className="py-3 px-4">
-                <span
-                    className={`px-3 py-1 rounded-full text-sm ${SlugStatus(props.Status)}`}
-                >
-                    {props.Status}
-                </span>
-            </td>
-            <td className="flex flex-row py-3 px-4">
-                {/* <Link */}
-                {/*     to={`/item/${props.id}`} */}
-                {/*     title="View more" */}
-                {/*     className="mr-2 text-2xl text-green-500" */}
-                {/* > */}
-                {/*     <MdVisibility /> */}
-                {/* </Link> */}
-                <ShowButtonIfUserAdmin
-                    userRole={userRole}
-                    onHandleArchiveItem={handleArchiveItem}
-                />
-            </td>
-            {isConfirmOpen && (
-                <PopUpModal
-                    title={"Archive Item"}
-                    label={"archive"}
-                    noun={"item"}
-                    destination={"archive"}
-                    onHandleCancelAction={handleCancelArchive}
-                    onHandleConfirmAction={handleConfirmArchive}
-                />
-            )}
+            <tr onClick={() => navigate(`/item/${props.id}`)}
+                className="transition-colors cursor-pointer odd:bg-white even:bg-[#f9fbff] hover:bg-[#f8fafc]">
+                <td className="py-3 px-4">{props.SerialNumber}</td>
+                <td className="py-3 px-4">
+                    <img
+                        src={typeof props.Image === "string" ? props.Image : logo}
+                        alt={props.ItemName}
+                        className="w-10 h-10 rounded-xl"
+                    />
+                </td>
+                <td className="py-3 px-4">{props.ItemName}</td>
+                <td className="py-3 px-4">{props.Category}</td>
+                <td className="py-3 px-4">
+                    <span
+                        className={`px-3 py-1 rounded-full text-sm ${SlugCondition(props.Condition)}`}
+                    >
+                        {props.Condition}
+                    </span>
+                </td>
+                <td className="py-3 px-4">{FormattedDateTime(props.createdAt)}</td>
+                <td className="py-3 px-4">
+                    <span
+                        className={`px-3 py-1 rounded-full text-sm ${SlugStatus(props.Status)}`}
+                    >
+                        {props.Status}
+                    </span>
+                </td>
+                <td onClick={(e) => e.stopPropagation()} className="flex flex-row py-3 px-4">
+                    <ShowButtonIfUserAdmin
+                        userRole={userRole}
+                        onHandleArchiveItem={handleArchiveItem}
+                    />
+                </td>
+            </tr>
+            {
+                isConfirmOpen && (
+                    <PopUpModal
+                        title={"Archive Item"}
+                        label={"archive"}
+                        noun={"item"}
+                        destination={"archive"}
+                        onHandleCancelAction={handleCancelArchive}
+                        onHandleConfirmAction={handleConfirmArchive}
+                    />
+                )
+            }
         </>
     );
 };
