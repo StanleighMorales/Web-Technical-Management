453a9ded9384757bc3a9440d65ca7e3f3541e954
Christian <christiandave120702@gmail.com>
2025-11-24 15:16:22 +0800

    refactor: fix the naming and the attribute for the api

--- src/query/patch/usePatchUserMutation.ts
+++ src/query/patch/usePatchUserMutation.ts
@@ -2,53 +2,63 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { getToken, removeToken } from "../../utils/token";
 
 type PathUserCredentials = {
-  username: string,
-  email: string,
-  phoneNumber: string,
-  lastName: string,
-  middleName: string,
-  firstName: string,
-  position: string
+    username: string,
+    email: string,
+    phoneNumber: string,
+    lastName: string,
+    middleName: string,
+    firstName: string,
+    userRole: string
 }
 
 type PatchUserProps = {
-  id: string;
-  formData: PathUserCredentials
+    id: string;
+    formData: PathUserCredentials
 }
 
 const PatchUser = async ({ id, formData }: PatchUserProps) => {
-  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-  const VERSION = "v1";
-  const END_POINT = `/api/${VERSION}/users/admin-or-staff/profile`;
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    const VERSION = "v1";
+    const END_POINT = `/api/${VERSION}/users/admin-or-staff/profile`;
 
-  const updatedUser = JSON.stringify(formData);
-  const res = await fetch(`${BASE_URL}${END_POINT}/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`
-    },
-    body: updatedUser
-  });
+    const updateProfile = {
+        username: formData.username,
+        email: formData.email,
+        phoneNumber: formData.phoneNumber,
+        lastName: formData.lastName,
+        middleName: formData.middleName,
+        firstName: formData.firstName,
+        position: formData.userRole
+    }
 
-  if (res.status === 401) {
-    removeToken();
-    return;
-  }
+    const updatedUser = JSON.stringify(updateProfile);
+    const res = await fetch(`${BASE_URL}${END_POINT}/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${getToken()}`
+        },
+        body: updatedUser
+    });
 
-  const data = await res.text()
-  if (!res.ok) throw new Error(data)
-  return data;
+    if (res.status === 401) {
+        removeToken();
+        return;
+    }
+
+    const data = await res.text()
+    if (!res.ok) throw new Error(data)
+    return data;
 
 }
 export const usePatchUserMutation = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationKey: ["admin-or-staff"],
-    mutationFn: PatchUser,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["me"] })
-      queryClient.invalidateQueries({queryKey: ["users"]})
-    }
-  })
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationKey: ["admin-or-staff"],
+        mutationFn: PatchUser,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["me"] })
+            queryClient.invalidateQueries({ queryKey: ["users"] })
+        }
+    })
 }
