959fd620cbe6fca3bd992fb49943e6fe86e97f40
Christian <christiandave120702@gmail.com>
2025-11-24 15:16:40 +0800

    refactor: fix the naming and the attribute for api

--- src/query/patch/usePatchTeacherMutation.ts
+++ src/query/patch/usePatchTeacherMutation.ts
@@ -3,52 +3,52 @@ import type { TUpdatedTeacher } from "../../types/types.ts";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type TeacherPatchProps = {
-  id: string;
-  formData: TUpdatedTeacher;
+    id: string;
+    formData: TUpdatedTeacher;
 };
 const TeacherPatch = async ({ id, formData }: TeacherPatchProps) => {
-  try {
-    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-    const VERSION = "v1";
-    const END_POINT = `/api/${VERSION}/users/teachers/profile`;
+    try {
+        const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+        const VERSION = "v1";
+        const END_POINT = `/api/${VERSION}/users/teachers/profile`;
 
-    const updateTeacher = JSON.stringify(formData);
-    const res = await fetch(`${BASE_URL}${END_POINT}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getToken()}`,
-      },
-      body: updateTeacher,
-    });
+        const updateTeacher = JSON.stringify(formData);
+        const res = await fetch(`${BASE_URL}${END_POINT}/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${getToken()}`,
+            },
+            body: updateTeacher,
+        });
 
-    if (res.status === 401) {
-      removeToken();
-      return;
+        if (res.status === 401) {
+            removeToken();
+            return;
+        }
+
+        if (res.status === 404) {
+            throw new Error("Submission failed");
+        }
+
+        const data = await res.json();
+
+        return data.data;
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
     }
-
-    if (res.status === 404) {
-      throw new Error("Submission failed");
-    }
-
-    const data = await res.json();
-
-    return data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.message);
-    }
-  }
 };
 
 export const usePatchTeacherMutation = () => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  return useMutation({
-    mutationKey: ["profile"],
-    mutationFn: TeacherPatch,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["teachers"] });
-    },
-  });
+    return useMutation({
+        mutationKey: ["profile"],
+        mutationFn: TeacherPatch,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["teachers"] });
+        },
+    });
 };
