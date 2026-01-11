60b9bef205aaf0633950e5a2d721abebd03f53b0
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-11 14:30:27 +0800

    feat: add user hooks for managing user data and actions

---
+++ src/hooks/userHooks.ts
@@ -0,0 +1,158 @@
+import {
+  queryOptions,
+  useQueryClient,
+  useMutation,
+} from "@tanstack/react-query";
+import {
+  allUsersApi,
+  allUsersArchiveApi,
+  archiveUserApi,
+  deleteUserApi,
+  getArchiveUserInfo,
+  userLoggedIn,
+  resetPasswordUser,
+  registerUserApi,
+  restoreUserApi,
+  updateStudentApi,
+  updateTeacherApi,
+  updateUserApi,
+} from "../api/user_api";
+
+export const useAllUsers = () => {
+  return queryOptions({
+    queryFn: allUsersApi,
+    queryKey: ["users"],
+  });
+};
+
+export const useAllUsersArchive = () => {
+  return queryOptions({
+    queryFn: allUsersArchiveApi,
+    queryKey: ["archiveusers"],
+  });
+};
+
+export const useArchiveUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: archiveUserApi,
+    mutationKey: ["archive"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      queryClient.invalidateQueries({ queryKey: ["teachers"] });
+      queryClient.invalidateQueries({ queryKey: ["students"] });
+    },
+  });
+};
+
+export const useDeleteUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteUserApi,
+    mutationKey: ["archiveusers"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["archiveusers"] });
+    },
+    onError: (error) => {
+      console.log(error.message);
+    },
+  });
+};
+
+export const useGetArchiveUserInfo = (id: string) => {
+  return queryOptions({
+    queryFn: () => getArchiveUserInfo(id),
+    queryKey: ["ArchiveUsers", id],
+  });
+};
+
+export const useLoggedInUser = () => {
+  return queryOptions({
+    queryFn: userLoggedIn,
+    queryKey: ["me"],
+  });
+};
+
+export const useResetUserPassword = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: resetPasswordUser,
+    mutationKey: ["change-password"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+    onError: (error) => {
+      console.log(error.message);
+    },
+  });
+};
+
+export const useRegisterUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: registerUserApi,
+    mutationKey: ["register"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+    onError: (error: any) => {
+      if (error.response) console.log(error.response.data.Errors);
+    },
+  });
+}
+
+export const useRestoreUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: restoreUserApi,
+    mutationKey: ["restore"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["archiveusers"] });
+    },
+    onError: (error) => {
+      console.log(error.message);
+    },
+  });
+};
+
+export const useUpdateStudent = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateStudentApi,
+    mutationKey: ["profile"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["students"] });
+    },
+  });
+};
+
+export const useUpdateTeacher = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateTeacherApi,
+    mutationKey: ["profile"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["teachers"] });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateUserApi,
+    mutationKey: ["admin-or-staff"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      queryClient.invalidateQueries({ queryKey: ["me"] });
+    },
+  });
+};
