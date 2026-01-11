24ddbb1854e15fcad8b4f492e9e366f20e480565
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-11 13:58:13 +0800

    feat: implement item management hooks for CRUD operations

---
+++ src/hooks/itemHooks.ts
@@ -0,0 +1,166 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { queryOptions } from "@tanstack/react-query";
+import {
+  addItemApi,
+  allItemsApi,
+  allItemsArchiveApi,
+  archiveItemApi,
+  borrowItem,
+  deleteItemApi,
+  getArchiveItemInfo,
+  getItemApi,
+  importItem,
+  recentlyBorrowItems,
+  restoreItemApi,
+  returnItem,
+  updateItemApi,
+} from "../api/item_api";
+
+export const useAllItems = () => {
+  return queryOptions({
+    queryFn: allItemsApi,
+    queryKey: ["items"],
+  });
+};
+
+export const useAddItem = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: addItemApi,
+    mutationKey: ["items"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["items"] });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useUpdateItem = () => {
+  const querClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateItemApi,
+    mutationKey: ["items"],
+    onSuccess: () => {
+      querClient.invalidateQueries({ queryKey: ["items"] });
+    },
+    onError: (error) => {
+      console.log(error.message);
+    },
+  });
+};
+export const useAllItemsArchive = () => {
+  return queryOptions({
+    queryFn: allItemsArchiveApi,
+    queryKey: ["archiveitems"],
+  });
+};
+
+export const useArchiveItem = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: archiveItemApi,
+    mutationKey: ["archive"],
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["items"] });
+      console.log(data.message);
+    },
+    onError: (error: any) => {
+      if (error.response) console.log(error.response.data.Errors);
+    },
+  });
+};
+
+export const useBorrowItem = () => {
+  return useMutation({
+    mutationFn: borrowItem,
+    mutationKey: ["lentitems"],
+  });
+};
+
+export const useDeleteItem = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteItemApi,
+    mutationKey: ["archiveitems"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["archiveitems"] });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useGetArchiveItemInfo = (id: string) => {
+  return queryOptions({
+    queryFn: () => getArchiveItemInfo(id),
+    queryKey: ["archiveitems", id],
+  });
+};
+
+export const useGetItemInfo = (id: string) => {
+  return queryOptions({
+    queryFn: () => getItemApi(id),
+    queryKey: ["items"],
+  });
+};
+
+export const useImportItem = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: importItem,
+    mutationKey: ["import"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["items"],
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useRecentlyBorrowItems = (id?: string) => {
+  return queryOptions({
+    queryFn: () => recentlyBorrowItems(id),
+    queryKey: ["lentItems", id],
+  });
+};
+
+export const useRestoreItem = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: restoreItemApi,
+    mutationKey: ["restore"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["archiveitems"] });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useReturnItem = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: returnItem,
+    mutationKey: ["returnItem"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["items"] });
+      queryClient.invalidateQueries({ queryKey: ["lentItems"] });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
