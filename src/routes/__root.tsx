5af73405a949177ba5b4bc2449273da02e21c08d
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-13 20:25:10 +0800

    feat: add root routing setup with protected routes for application components

---
+++ src/routes/__root.tsx
@@ -0,0 +1,104 @@
+import { createRoute, createRootRoute } from "@tanstack/react-router";
+
+import App from "../App";
+import Login from "./auth/Login";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import InventoryList from "./InventoryList";
+import UserManagement from "./UserManagement";
+import HistoryList from "./HistoryList";
+import Archive from "./Archive";
+import BorrowItem from "./BorrowItem";
+import PendingReservations from "./PendingReservations";
+import Settings from "./Settings";
+import Registration from "./Registration";
+import { ProtectedRoute } from "../utils/middleware/accessAuth";
+
+export const Route = createRootRoute({
+    component: App,
+});
+
+const indexRoute = createRoute({
+    getParentRoute: () => Route,
+    path: "/",
+    component: Login,
+});
+
+const homeRoute = createRoute({
+    getParentRoute: () => Route,
+    path: "home",
+    component: () => (
+        <ProtectedRoute>
+            <Home />
+        </ProtectedRoute>
+    ),
+});
+
+const DashboardRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "dashboard",
+    component: () => Dashboard,
+});
+
+const InventoryListRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "inventory-list",
+    component: () => InventoryList,
+});
+
+const UserManagementRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "user-management",
+    component: UserManagement,
+});
+
+const HistoryListRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "history-list",
+    component: HistoryList,
+});
+
+const ArchiveRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "archive-table",
+    component: Archive,
+});
+
+const BorrowItemRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "borrow-item",
+    component: BorrowItem,
+});
+
+const PendingReservationRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "pending-reservations",
+    component: PendingReservations,
+});
+
+const SettingsRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "settings",
+    component: Settings,
+});
+
+const RegistrationModuleRoute = createRoute({
+    getParentRoute: () => homeRoute,
+    path: "registration-module",
+    component: Registration,
+});
+
+export const routeTree = Route.addChildren([
+    indexRoute,
+    homeRoute.addChildren([
+        DashboardRoute,
+        InventoryListRoute,
+        UserManagementRoute,
+        HistoryListRoute,
+        ArchiveRoute,
+        BorrowItemRoute,
+        PendingReservationRoute,
+        SettingsRoute,
+        RegistrationModuleRoute,
+    ]),
+]);
