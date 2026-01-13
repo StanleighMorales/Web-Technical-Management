b73d4166d3ffc49e2a4eff378228264877d1a1f9
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-01-13 20:24:50 +0800

    refactor: implement new routing setup with createRouter and integrate NotFound component

--- src/main.tsx
+++ src/main.tsx
@@ -1,125 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "@tanstack/react-router";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NotFound from "./routes/NotFound";
+import { routeTree } from "./routes/__root";
 
-// import { ProtectedRoute, PublicRoute } from "./utils/middleware/accessAuth.tsx";
-// import Login from "./auth/Login.tsx";
-//
-// const App = lazy(() => import("./App.tsx"));
-// const Home = lazy(() => import("./layout/Home.tsx"));
-// const Dashboard = lazy(() => import("./pages/Dashboard.tsx"));
-// const InventoryList = lazy(() => import("./pages/InventoryList.tsx"));
-// const UserManagement = lazy(() => import("./pages/UserManagement.tsx"));
-// const HistoryList = lazy(() => import("./pages/HistoryList.tsx"));
-// const Settings = lazy(() => import("./pages/Settings.tsx"));
-// const ViewItem = lazy(() => import("./components/ViewItem.tsx"));
-// const Archive = lazy(() => import("./pages/Archive.tsx"));
-// const BorrowItem = lazy(() => import("./pages/BorrowItem.tsx"));
-// const PendingReservations = lazy(
-//     () => import("./pages/PendingReservations.tsx"),
-// );
-// const Registration = lazy(() => import("./pages/Registration.tsx"));
-// const NotFound = lazy(() => import("./pages/NotFound.tsx"));
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  defaultNotFoundComponent: NotFound,
+});
 
-// const routes = createBrowserRouter([
-//     // Public routes
-//     {
-//         path: "/",
-//         element: (
-//             <PublicRoute>
-//                 <App />
-//             </PublicRoute>
-//         ),
-//         children: [
-//             {
-//                 index: true,
-//                 element: <Login />,
-//             },
-//         ],
-//     },
-//     // Protected Routes
-//     {
-//         path: "/home",
-//         element: (
-//             <ProtectedRoute>
-//                 <Home />
-//             </ProtectedRoute>
-//         ),
-//         children: [
-//             {
-//                 path: "dashboard",
-//                 element: (
-//                     <Dashboard />
-//                 ),
-//             },
-//             {
-//                 path: "inventory-list",
-//                 element: (
-//                     <InventoryList />
-//                 ),
-//             },
-//             {
-//                 path: "user-management",
-//                 element: (
-//                     <UserManagement />
-//                 ),
-//             },
-//             {
-//                 path: "history-list",
-//                 element: (
-//                     <HistoryList />
-//                 ),
-//             },
-//             {
-//                 path: "archive-table",
-//                 element: (
-//<Archive />
-//                 ),
-//             },
-//             {
-//                 path: "borrow-item",
-//                 element: (
-//                     <BorrowItem />
-//                 ),
-//             },
-//             {
-//                 path: "pending-reservations",
-//                 element: (
-//                     <PendingReservations />
-//                 ),
-//             },
-//             {
-//                 path: "settings",
-//                 element: (
-//                     <Settings />
-//                 ),
-//             },
-//             {
-//                 path: "registration-module",
-//                 element: (
-//                     <Registration />
-//                 ),
-//             },
-//         ],
-//     },
-//
-//     {
-//         path: "/item/:id",
-//         element: (
-//             <ProtectedRoute>
-//                 <ViewItem />
-//             </ProtectedRoute>
-//         ),
-//     },
-//     {
-//         path: "*",
-//         element: <NotFound />,
-//     },
-// ]);
-
-import { router } from "../src/router/__roots";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
