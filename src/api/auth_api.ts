ba3bc82815c83255c7d4f1e69302d8316e478ca2
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-02-19 18:12:35 +0800

    refactor: update API endpoint paths for login and logout functions to remove versioning

--- src/api/auth_api.ts
+++ src/api/auth_api.ts
@@ -3,21 +3,12 @@ import type { TLoginUser } from "../types/types";
 import { removeToken } from "../utils/token";
 
 export const LoginUserApi = async (data: TLoginUser) => {
-  try {
-    const response = await api.post("/api/v1/auth/login", data);
+    const response = await api.post("/auth/login", data);
     return response.data;
-  } catch (error: any) {
-    if (error.response) {
-      const errors = error.response.data.Errors;
-      throw new Error(errors);
-    } else {
-      throw new Error(error.message);
-    }
-  }
 };
 
 export const logoutUserApi = async () => {
-  const response = await api.post("/api/v1/auth/logout");
+  const response = await api.post("/auth/logout");
   if (response) {
     removeToken();
   }
