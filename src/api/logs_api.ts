1b1dad44d81a8104519f61ea26ba364ba46932b8
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-04-22 17:50:45 +0800

    refactor: migrate type definitions to @types directory and implement BorrowLogs route

--- src/api/logs_api.ts
+++ src/api/logs_api.ts
@@ -5,3 +5,8 @@ export const activityLogsApi = async () => {
     const response = await api.get("/activity-logs");
     return response.data;
 };
+
+export const borrowLogsApi = async () => {
+    const response = await api.get("/activity-logs/borrow-logs");
+    return response.data;
+}
