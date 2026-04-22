70fd9ee95f0093ff825dfb427eacc8eadec9b057
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-04-22 17:51:24 +0800

    refactor: remove central types file and implement BorrowLogs route with associated data definitions

--- src/hooks/logsHooks.ts
+++ src/hooks/logsHooks.ts
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { activityLogsApi } from "../api/logs_api";
+import { activityLogsApi, borrowLogsApi } from "../api/logs_api";
 
 
 export const useActivityLogs = () => {
     return useQuery({
         queryKey: ["activity-logs"],
         queryFn: activityLogsApi,
+    });
+};
+
+export const useBorrowLogs = () => {
+    return useQuery({
+        queryKey: ["borrow-logs"],
+        queryFn: borrowLogsApi,
     });
 };
